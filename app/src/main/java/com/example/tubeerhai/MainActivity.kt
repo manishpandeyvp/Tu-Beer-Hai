80c488979c2120514f70965a178b789682207434
manishkumarpandey <manishpandeyvp@gmail.com>
2023-05-07 07:51:17 +0530

    Desc - Completed Setup for Recycler View

--- app/src/main/java/com/example/tubeerhai/MainActivity.kt
+++ app/src/main/java/com/example/tubeerhai/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.tubeerhai
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import androidx.activity.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tubeerhai.data.model.BeerModel
 import com.example.tubeerhai.presentation.BeersViewModel
+import com.example.tubeerhai.presentation.adapter.BeersListAdapter
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -15,10 +19,18 @@ import kotlinx.coroutines.launch
 class MainActivity : AppCompatActivity() {
 
     private val beersViewModel: BeersViewModel by viewModels()
+    private lateinit var rvBeerList: RecyclerView
+    private var beersList: List<BeerModel> = emptyList()
+    private lateinit var adapter: BeersListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvBeerList = findViewById(R.id.rv_beers_list)
+        adapter = BeersListAdapter(this, beersList)
+        setUpRecyclerView()
+
         beersViewModel.callGetBeerListApi()
         postFetchingBeersList()
     }
@@ -29,15 +41,36 @@ class MainActivity : AppCompatActivity() {
                 when {
                     value.isLoading -> {
                         Log.d("list_debug: ", value.isLoading.toString())
+                        rvBeerList.visibility = View.GONE
                     }
                     value.error.isNotBlank() -> {
                         Log.d("list_debug: ", value.error)
+                        rvBeerList.visibility = View.GONE
                     }
                     value.beersList.isEmpty().not() -> {
                         Log.d("list_debug: ", value.beersList.toString())
+                        updateBeersListUi(value.beersList)
                     }
                 }
             }
+        }
+    }
+
+    private fun setUpRecyclerView() {
+        rvBeerList.layoutManager = LinearLayoutManager(this)
+        rvBeerList.adapter = adapter
+        adapter.setOnClickListener(object : BeersListAdapter.OnClickListener {
+            override fun onClick(position: Int, model: BeerModel) {
+                Log.d("list_debug_click: ", "$model")
+            }
+        })
+    }
+
+    private fun updateBeersListUi(list: List<BeerModel>) {
+        if (list.isNotEmpty()) {
+            beersList = list
+            rvBeerList.visibility = View.VISIBLE
+            adapter.setData(beersList)
         }
     }
 }
--- app/src/main/java/com/example/tubeerhai/presentation/adapter/BeersListAdapter.kt
+++ app/src/main/java/com/example/tubeerhai/presentation/adapter/BeersListAdapter.kt
@@ -1,9 +1,12 @@
 package com.example.tubeerhai.presentation.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.tubeerhai.R
@@ -11,19 +14,47 @@ import com.example.tubeerhai.data.model.BeerModel
 
 class BeersListAdapter(
     private val context: Context,
-    private val list: List<BeerModel>
-): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return MyViewHolder(LayoutInflater.from(context).inflate(R.layout.item_beer_layout, parent, false))
+    private var list: List<BeerModel>
+): RecyclerView.Adapter<BeersListAdapter.BeersViewHolder>() {
+
+    inner class BeersViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val beerImage: ImageView = view.findViewById(R.id.iv_beer_image)
+        val beerName: TextView = view.findViewById(R.id.tv_title)
+        val beerTagLine: TextView = view.findViewById(R.id.tv_tag_line)
+        val beerShare: ImageView = view.findViewById(R.id.iv_share)
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val model = list[position]
+    private var onLongClickListener: OnClickListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BeersViewHolder {
+        return BeersViewHolder(LayoutInflater.from(context).inflate(R.layout.item_beer_layout, parent, false))
     }
 
     override fun getItemCount(): Int {
         return list.size
     }
 
-    private class MyViewHolder(view: View): RecyclerView.ViewHolder(view)
+    override fun onBindViewHolder(holder: BeersViewHolder, position: Int) {
+        val model = list[position]
+        Glide.with(context).load(model.imageUrl).centerInside().into(holder.beerImage)
+        holder.beerName.text = model.name
+        holder.beerTagLine.text = model.tagline
+        holder.beerShare.setOnClickListener {
+            onLongClickListener?.onClick(position, model)
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun setData(list: List<BeerModel>) {
+        this.list = list
+        notifyDataSetChanged()
+    }
+
+    interface OnClickListener {
+        fun onClick(position: Int, model: BeerModel)
+    }
+
+    fun setOnClickListener(onClickListener: OnClickListener) {
+        this.onLongClickListener = onClickListener
+    }
 }
